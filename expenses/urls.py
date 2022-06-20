25670c3c8f33c84c53f0bf4077ed8b5cb301533c
rahulkumar161098 <rahulkumar161098@gmail.com>
2022-06-20 13:59:35 +0530

    models

--- expenses/urls.py
+++ expenses/urls.py
@@ -4,6 +4,8 @@ from expenses import views
 
 urlpatterns = [
     path('dashboard', views.dashboard, name="dashboard"),
-    path('add_expences', views.addExpence, name="add_expences")
+    path('add_expences', views.addExpence, name="add_expences"),
+    path('edit_expense/<int:pk>', views.edit_expense, name="edit_expense"),
+    path('delete_expense/<int:pk>', views. delete_expense, name="delete_expense")
     
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,8 @@
-from ast import Add
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from expenses.models import AddExpence
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 def dashboard(request):
@@ -12,6 +13,17 @@ def base_page(request):
 
 def addExpence(request):
     current_user= request.user
+    expence_data= AddExpence.objects.filter(owner=current_user)
+
+    paginator= Paginator(expence_data, 2)
+    page_number= request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+    print(page_obj)
+    context={
+        'expence_user_data': expence_data,
+        'page_obj': page_obj
+    }
+
     if request.method== 'POST':
         amount= request.POST['amount']
         des= request.POST['des']
@@ -23,6 +35,39 @@ def addExpence(request):
             messages.info(request,'Your expence is added successfully')
             return redirect('add_expences')
         except:
+            messages .error(request, 'Something went wrong')
             return render(request, 'addExpences.html')
 
-    return render(request, 'addExpences.html')
+    return render(request, 'addExpences.html', context)
+
+def edit_expense(request, pk):
+    if not request.user.is_authenticated:
+        return redirect ('user_log')
+    edit_expense= AddExpence.objects.get(id=pk)
+    if request.method== 'POST':
+        amount= request.POST['amount']
+        des= request.POST['des']
+        exp_category= request.POST['expence_category']
+        date= request.POST['date']
+
+        edit_expense.amount=amount
+        edit_expense.date= date
+        edit_expense.description= des
+        edit_expense.category= exp_category
+        try:
+            edit_expense.save()
+            messages.info(request, "Expense update successfully")
+            return redirect('add_expences')
+        except:
+            messages.info(request, "Somethings Went Wrong!")
+            return render(request, 'expenses/edit_expense.html')
+    # print('........................',edit_expense.owner)
+    return render(request, 'expenses/edit_expense.html', locals())
+
+def delete_expense(request, pk):
+    if not request.user.is_authenticated:
+        return redirect ('user_log')
+    delete_expense= AddExpence.objects.get(id=pk)
+    delete_expense.delete()
+    return redirect('add_expences')
+    
